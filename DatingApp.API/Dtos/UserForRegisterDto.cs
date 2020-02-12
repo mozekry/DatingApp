e3ac1298f24ec84dacc562e36fd59814d5a926f8
Mohamed Zekry <mohamed.zekry@z2data.com>
2020-02-12 19:02:23 +0200

    Add login action and create login token

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string userName { get; set; }
+        [Required]
+        [StringLength(8,MinimumLength = 4,ErrorMessage ="password between 4 and 8 characters")]
+        public string password { get; set; }
+    }
+}
