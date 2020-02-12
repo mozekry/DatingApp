aad5ff977f0596732af41c4b738086b54f375724
mozekry <mozekry@gmail.com>
2020-02-12 19:39:26 +0200

    add Authorization to pipeline

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -15,7 +15,7 @@ export class ValueComponent implements OnInit {
   }
 
   getValues(){
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+    this.http.get('http://localhost:44797/api/values').subscribe(response => {
       this.values = response;
       console.log(this.values);
     }, error => {
